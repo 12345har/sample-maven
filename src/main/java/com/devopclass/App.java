11d55855eca08e337809497d53e5d54253e9a1f6
EC2 Default User <ec2-user@ip-172-31-39-74.ap-south-1.compute.internal>
2022-05-06 12:17:37 +0000

    commet one word

--- src/main/java/com/devopclass/App.java
+++ src/main/java/com/devopclass/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! again hello world" );
+        System.out.println( "Hello World! again hello world thi is countinu the prosess" );
     }
 }
